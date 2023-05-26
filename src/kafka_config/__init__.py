5d6eca420f0b43b4c6947f422c5e8bac75c25d02
Chandra Reddy Tiyyagura <chandra.tiyyagura@gmail.com>
2023-05-26 21:53:22 +0530

    kafka config folder created

--- src/kafka_config/__init__.py
+++ src/kafka_config/__init__.py
@@ -5,10 +5,11 @@ import os
 API_KEY = os.getenv('API_KEY',None)
 API_SECRET_KEY = os.getenv('API_SECRET_KEY',None)
 BOOTSTRAP_SERVER = os.getenv('BOOTSTRAP_SERVER',None)
-# 
+# Schema keys
 SCHEMA_REGISTRY_API_KEY = os.getenv('SCHEMA_REGISTRY_API_KEY',None)
 SCHEMA_REGISTRY_API_SECRET = os.getenv('SCHEMA_REGISTRY_API_SECRET',None)
 ENDPOINT_SCHEMA_URL = os.getenv('ENDPOINT_SCHEMA_URL',None)
+# Security and SSL
 SECURITY_PROTOCOL = os.getenv('SECURITY_PROTOCOL',None)
 SSL_MACHENISM = os.getenv('SSL_MACHENISM',None)
 
